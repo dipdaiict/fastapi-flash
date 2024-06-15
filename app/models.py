921a35184386c9d84fb45dff3bfba4c0c814ba5f
Dip Patel <dippatel256@gmail.com>
2024-06-15 23:51:07 +0530

    posts-user relationship added.

--- app/models.py
+++ app/models.py
@@ -12,8 +12,10 @@ class Post(Base):
     id  = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='TRUE', nullable=False)   # By Default it takes True if not provide then...
+    published = Column(Boolean, server_default='TRUE', nullable=False) 
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('NOW()'))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    user = relationship("User")
     
 class User(Base):
     __tablename__ = "users"
