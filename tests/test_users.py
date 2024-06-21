90c64b7e2ec80ad081a87314344cd3ee88503b1c
Dip Patel <dippatel256@gmail.com>
2024-06-21 17:12:41 +0530

    user test added.

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,8 @@
-from fastapi.testclient import TestClient
+import pytest
+from jose import jwt
 from app import schemas
+from app.config import settings
+from fastapi.testclient import TestClient
 
 def test_root(client: TestClient):
     response = client.get("/")
@@ -41,3 +44,33 @@ def test_create_user_username_exists(client: TestClient):
     })
     assert response.status_code == 400
     assert response.json() == {"detail": "Username already exists."}
+    
+@pytest.fixture(scope="module")
+def test_user(client):
+    user_data = {"email": "testlogin@example.com", "password": "password123", "username": "testlogin"}
+    res = client.post("/user/createuser", json=user_data)
+    assert res.status_code == 201
+    return {"id": res.json()["id"], "email": user_data["email"], "password": user_data["password"]}
+
+def test_login_user(test_user, client):
+    data={"email": test_user['email'], "password": test_user['password']}
+    res = client.post("/authentication/login", json=data)
+    assert res.status_code == 200
+    login_res = res.json()
+    assert "access_token" in login_res
+    assert login_res["token_type"] == "Bearer"
+    token_data = jwt.decode(login_res["access_token"], settings.secret_key, algorithms=[settings.algorithm])
+    assert token_data.get("user_id") == test_user['id']
+    assert token_data.get("email") == test_user['email']
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 404),  
+    ('testlogin@example.com', 'wrongpassword', 404), 
+    ('wrongemail@gmail.com', 'wrongpassword', 404),  
+    (None, 'password123', 404),
+    ('testlogin@example.com', None, 404)  
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"email": email, "password": password})
+    assert res.status_code == status_code
+
