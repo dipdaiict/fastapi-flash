2cef74cc595a55b67e2719dda6eff95dbd783203
Dip Patel <dippatel256@gmail.com>
2024-06-26 23:45:04 +0530

    Updated Workflow and main.py

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
           
 @app.get("/")  
 def read_root():  
-    return {"Message": "Welcome to the Home Page. CICD Works"} 
+    return {"Message": "Welcome to the Home Page. CICD Implemented."} 
 
 app.include_router(posts.router)
 app.include_router(users.router)
